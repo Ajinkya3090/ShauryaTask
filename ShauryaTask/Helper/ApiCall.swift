787a5a4956c4ae98b27b93d6db426cd620f83f83
Ajinkya Andhare <ajinkyaa@bipp.io>
2022-09-16 14:13:04 +0530

    intial Commit

--- ShauryaTask/Helper/ApiCall.swift
+++ ShauryaTask/Helper/ApiCall.swift
@@ -13,10 +13,8 @@ class ApiCall {
         guard let url = URL(string: url) else{ return }
         URLSession(configuration: .default).dataTask(with: url) {  data, response, error in
             guard let data = data else{ return }
-            print(data)
             do{
                 let userData = try JSONDecoder().decode(EmployeeData.self, from: data)
-                print(userData)
                 completion(userData)
             }
             catch{
